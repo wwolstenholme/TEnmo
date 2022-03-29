44cac8de9d8dbd82dd11c70ba6d23bf77b7a661b
wwolstenholme <97864567+wwolstenholme@users.noreply.github.com>
2022-03-28 21:40:27 -0400

    Add files via upload

---
+++ TenmoClient/Models/TenmoAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TenmoClient.Models
+{
+    public class TenmoAccount
+    {
+        public decimal accountBalance { get; set; } = 1000M;
+
+        public int accountId { get; private set; }
+
+        public TenmoAccount()
+        {
+
+        }
+
+        public TenmoAccount(decimal accountBalance, int accountId)
+        {
+            this.accountBalance = accountBalance;
+            this.accountId = accountId;
+        }
+    }
+}
---
+++ TenmoClient/Services/TenmoApiService.cs
@@ -0,0 +1,81 @@
+﻿using RestSharp;
+using System.Collections.Generic;
+using TenmoClient.Models;
+
+namespace TenmoClient.Services
+{
+    public class TenmoApiService : AuthenticatedApiService
+    {
+        public readonly string ApiUrl;
+
+        public TenmoApiService(string apiUrl) : base(apiUrl) { }
+
+        public decimal GetBalance()
+        {
+            //no endpoint for account balance
+            RestRequest request = new RestRequest($"account");
+            IRestResponse<decimal> response = client.Get<decimal>(request);
+            CheckForError(response);
+            return response.Data;
+        }
+
+        //public List<TenmoAccount> GetAccountsSearchName(string searchTerm)
+
+        //public TenmoAccount UpdateBalance(TenmoAccount accountToUpdate)
+        //{
+        //    RestRequest request = new RestRequest($"account/{accountToUpdate.accountId}");
+        //    request.AddJsonBody(accountToUpdate);
+        //    IRestResponse<TenmoAccount> response = client.Put<TenmoAccount>(request);
+        //    CheckForError(response);
+        //    return response.Data;
+        //}
+
+        public void UpdateBalance(Transfer transfer)
+        {
+            RestRequest request = new RestRequest($"account");
+            request.AddJsonBody(transfer);
+            IRestResponse response = client.Put(request);
+            CheckForError(response);
+        }
+
+        public List<Transfer> ViewAllTransfers()
+        {
+            RestRequest request = new RestRequest($"transfer");
+            IRestResponse<List<Transfer>> response = client.Get<List<Transfer>>(request);
+            CheckForError(response);
+            return response.Data;
+        }
+
+        public Transfer CreateTransfer(Transfer transfer)
+        {
+            RestRequest request = new RestRequest($"transfer/{transfer.transferId}");
+            request.AddJsonBody(transfer);
+            IRestResponse<Transfer> response = client.Post<Transfer>(request);
+            CheckForError(response);
+            return response.Data;
+        }
+
+
+        public List<ApiUser> GetUsers()
+        {
+            RestRequest request = new RestRequest($"User");
+            IRestResponse<List<ApiUser>> response = client.Get<List<ApiUser>>(request);
+            CheckForError(response);
+            return response.Data;
+
+
+        }
+
+        public int GetAccountFromId(string userName)
+        {
+            RestRequest request = new RestRequest($"User");
+            request.AddJsonBody(userName);
+            IRestResponse<int> response = client.Get<int>(request);
+            CheckForError(response);
+            return response.Data;
+
+        }
+
+
+    }
+}
